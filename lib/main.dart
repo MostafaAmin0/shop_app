cdca11c3b32d5b37a321f854687e12ec036c69fc
MostafaAmin0 <mostafa.aminmk@gmail.com>
2021-10-10 06:06:30 +0200

    provider package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/product_overview_screen.dart';
+import './providers/products_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,17 +13,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Shop App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
-            .copyWith(secondary: Colors.deepOrange),
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (ctx) => ProductsProvider(),
+      child: MaterialApp(
+        title: 'Shop App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+              .copyWith(secondary: Colors.deepOrange),
+          fontFamily: 'Lato',
+        ),
+        routes: {
+          '/': (ctx) => ProductOverviewScreen(),
+          ProductDetailScreen.route: (ctx) => const ProductDetailScreen(),
+        },
       ),
-      routes: {
-        '/': (ctx) => ProductOverviewScreen(),
-        ProductDetailScreen.route: (ctx) =>const ProductDetailScreen(),
-      },
     );
   }
 }
