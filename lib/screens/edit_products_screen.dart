ce41552f754597da354c5cbebb0cb66879f05f48
MostafaAmin0 <mostafa.aminmk@gmail.com>
2021-10-22 14:23:25 +0200

    image preview

--- lib/screens/edit_products_screen.dart
+++ lib/screens/edit_products_screen.dart
@@ -1,24 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/widget/main_drawer.dart';
 
 class EditProductsScreen extends StatefulWidget {
   const EditProductsScreen({Key? key}) : super(key: key);
 
-  static const route ='/edit-product';
+  static const route = '/edit-product';
 
   @override
   _EditProductsScreenState createState() => _EditProductsScreenState();
 }
 
 class _EditProductsScreenState extends State<EditProductsScreen> {
-
   final _priceFocusNode = FocusNode();
-  final _describtionNode=FocusNode();
+  final _describtionNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+  final _imageUrlFocusNode = FocusNode();
+
+  void _updateImageUrl() {
+    if (!_imageUrlFocusNode.hasFocus) {
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _imageUrlFocusNode.addListener(_updateImageUrl);
+  }
 
   @override
   void dispose() {
     super.dispose();
+    _imageUrlFocusNode.removeListener(_updateImageUrl);
     _priceFocusNode.dispose();
     _describtionNode.dispose();
+    _imageUrlController.dispose();
+    _imageUrlFocusNode.dispose();
   }
 
   @override
@@ -27,6 +44,7 @@ class _EditProductsScreenState extends State<EditProductsScreen> {
       appBar: AppBar(
         title: const Text('Edit Products'),
       ),
+      drawer: const MainDrawer(),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
@@ -36,7 +54,7 @@ class _EditProductsScreenState extends State<EditProductsScreen> {
                 TextFormField(
                   decoration: const InputDecoration(labelText: 'Title'),
                   textInputAction: TextInputAction.next,
-                  onFieldSubmitted: (_){
+                  onFieldSubmitted: (_) {
                     FocusScope.of(context).requestFocus(_priceFocusNode);
                   },
                 ),
@@ -44,7 +62,7 @@ class _EditProductsScreenState extends State<EditProductsScreen> {
                   decoration: const InputDecoration(labelText: 'Price'),
                   textInputAction: TextInputAction.next,
                   keyboardType: TextInputType.number,
-                  onFieldSubmitted: (_){
+                  onFieldSubmitted: (_) {
                     FocusScope.of(context).requestFocus(_describtionNode);
                   },
                   focusNode: _priceFocusNode,
@@ -55,6 +73,45 @@ class _EditProductsScreenState extends State<EditProductsScreen> {
                   keyboardType: TextInputType.multiline,
                   focusNode: _describtionNode,
                 ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      margin: const EdgeInsets.only(top: 8, right: 10),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 1,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      child: _imageUrlController.text.isEmpty
+                          ? const Center(child: Text('Enter Url'))
+                          : FittedBox(
+                              child: Image.network(
+                                _imageUrlController.text,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Image Url',
+                        ),
+                        keyboardType: TextInputType.url,
+                        textInputAction: TextInputAction.done,
+                        controller: _imageUrlController,
+                        focusNode: _imageUrlFocusNode,
+                        ///works for me without these lines
+                        // onEditingComplete: () {
+                        //   setState(() {});
+                        // },
+                      ),
+                    ),
+                  ],
+                )
               ],
             ),
           ),
