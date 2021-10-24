a58593a963c9a9be24f65cfba84c7fce2837b273
MostafaAmin0 <mostafa.aminmk@gmail.com>
2021-10-24 23:56:59 +0200

    submit & validate values

--- lib/screens/edit_products_screen.dart
+++ lib/screens/edit_products_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import '../providers/product.dart';
 import 'package:shop_app/widget/main_drawer.dart';
 
 class EditProductsScreen extends StatefulWidget {
@@ -15,9 +17,22 @@ class _EditProductsScreenState extends State<EditProductsScreen> {
   final _describtionNode = FocusNode();
   final _imageUrlController = TextEditingController();
   final _imageUrlFocusNode = FocusNode();
+  final _form = GlobalKey<FormState>();
+  // var product =Product(id: null, title: '', description: '', price: 0, imageUrl: '');
+  late String title;
+  late String describtion;
+  late String imageUrl;
+  late double price;
 
   void _updateImageUrl() {
     if (!_imageUrlFocusNode.hasFocus) {
+      if (!(_imageUrlController.text.startsWith('http') ||
+              _imageUrlController.text.startsWith('https')) ||
+          !(_imageUrlController.text.endsWith('png') ||
+              _imageUrlController.text.endsWith('jpeg') ||
+              _imageUrlController.text.endsWith('jpg'))) {
+        return;
+      }
       setState(() {});
     }
   }
@@ -38,16 +53,31 @@ class _EditProductsScreenState extends State<EditProductsScreen> {
     _imageUrlFocusNode.dispose();
   }
 
+  void _saveFrom() {
+    final isValid = _form.currentState!.validate();
+    if (!isValid) {
+      return;
+    }
+    _form.currentState!.save();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Edit Products'),
+        actions: [
+          IconButton(
+            onPressed: _saveFrom,
+            icon: const Icon(Icons.save),
+          ),
+        ],
       ),
       drawer: const MainDrawer(),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
+          key: _form,
           child: SingleChildScrollView(
             child: Column(
               children: [
@@ -56,6 +86,15 @@ class _EditProductsScreenState extends State<EditProductsScreen> {
                   textInputAction: TextInputAction.next,
                   onFieldSubmitted: (_) {
                     FocusScope.of(context).requestFocus(_priceFocusNode);
+                  },
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Title can\'t be empty';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    title = value!;
                   },
                 ),
                 TextFormField(
@@ -66,12 +105,36 @@ class _EditProductsScreenState extends State<EditProductsScreen> {
                     FocusScope.of(context).requestFocus(_describtionNode);
                   },
                   focusNode: _priceFocusNode,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter price';
+                    } else if (double.tryParse(value) == null) {
+                      return 'Please enter valid number';
+                    } else if (double.parse(value) < 0) {
+                      return 'Please enter positive number';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    price = double.parse(value!);
+                  },
                 ),
                 TextFormField(
                   decoration: const InputDecoration(labelText: 'DescriPtion'),
                   maxLines: 3,
                   keyboardType: TextInputType.multiline,
                   focusNode: _describtionNode,
+                  onSaved: (value) {
+                    describtion = value!;
+                  },
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your description';
+                    } else if (value.length < 10) {
+                      return 'it has to be at least 10 characters long';
+                    }
+                    return null;
+                  },
                 ),
                 Row(
                   crossAxisAlignment: CrossAxisAlignment.end,
@@ -104,6 +167,24 @@ class _EditProductsScreenState extends State<EditProductsScreen> {
                         textInputAction: TextInputAction.done,
                         controller: _imageUrlController,
                         focusNode: _imageUrlFocusNode,
+                        onFieldSubmitted: (_) => _saveFrom(),
+                        onSaved: (value) {
+                          imageUrl = value!;
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter your image url';
+                          } else if (!(value.startsWith('https') ||
+                              value.startsWith('http'))) {
+                            return 'Please enter valid url with https';
+                          } else if (!(value.endsWith('png') ||
+                              value.endsWith('jpg') ||
+                              value.endsWith('jpeg'))) {
+                            return 'please enter image';
+                          }
+                          return null;
+                        },
+
                         ///works for me without these lines
                         // onEditingComplete: () {
                         //   setState(() {});
